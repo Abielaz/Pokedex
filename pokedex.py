26a0ebcacd001ccf050e4871206b4b54c07ef30b
Abielcz <abielczamora@gmail.com>
2026-06-20 03:53:43 -0400

    Add initial implementation of Pokedex app with Home, Search, Results, and Stats screens; include styling for UI components

--- pokedex.py
+++ pokedex.py
@@ -3,17 +3,23 @@ import json
 from textual.app import App, ComposeResult
 from textual.screen import Screen
 from textual.widgets import Button, Label, Input, Static
+from textual.containers import Center
 from pokemon.master import catch_em_all, get_pokemon
 
-asciiPokemon = catch_em_all()
+asciiPokemon = catch_em_all() #loads ASCII database so that it doesn't need to load with each search
 
-class HomePage(Screen):
+class HomePage(Screen): #each Screen subclass represents one full page of the app
+    CSS_PATH = "style.tcss"
     def compose(self) -> ComposeResult:
-        yield Static("                                  ,'\\\n    _.----.        ____         ,'  _\\   ___    ___     ____\n_,-'       `.     |    |  /`.   \\,-'    |   \\  /   |   |    \\  |`.\n\\      __    \\    '-.  | /   `.  ___    |    \\/    |   '-.   \\ |  |\n \\.    \\ \\   |  __  |  |/    ,','_  `.  |          | __  |    \\|  |\n   \\    \\/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |\n    \\     ,-'/  /   \\    ,'   | \\/ / ,`.|         /  /   \\  |     |\n     \\    \\ |   \\_/  |   `-.  \\    `'  /|  |    ||   \\_/  | |\\    |\n      \\    \\ \\      /       `-.`.___,-' |  |\\  /| \\      /  | |   |\n       \\    \\ `.__,'|  |`-._    `|      |__| \\/ |  `.__,'|  | |   |\n        \\_.-'       |__|    `-._ |              '-.|     '-.| |   |\n                                `'                            '-._|")
+        self.notify("App Works Best When Maximized! ^-^", title = "Quick Tip:", severity="information", timeout=10) #notification added for end-user tip
+        with Center():
+            yield Static("                                  ,'\\\n    _.----.        ____         ,'  _\\   ___    ___     ____\n_,-'       `.     |    |  /`.   \\,-'    |   \\  /   |   |    \\  |`.\n\\      __    \\    '-.  | /   `.  ___    |    \\/    |   '-.   \\ |  |\n \\.    \\ \\   |  __  |  |/    ,','_  `.  |          | __  |    \\|  |\n   \\    \\/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |\n    \\     ,-'/  /   \\    ,'   | \\/ / ,`.|         /  /   \\  |     |\n     \\    \\ |   \\_/  |   `-.  \\    `'  /|  |    ||   \\_/  | |\\    |\n      \\    \\ \\      /       `-.`.___,-' |  |\\  /| \\      /  | |   |\n       \\    \\ `.__,'|  |`-._    `|      |__| \\/ |  `.__,'|  | |   |\n        \\_.-'       |__|    `-._ |              '-.|     '-.| |   |\n                                `'                            '-._|", id="asciiHome")
         self.start = Button("Choose Your Pokemon!")
-        yield self.start
+        with Center():
+            yield self.start
         self.quit = Button("Quit")
-        yield self.quit
+        with Center():
+            yield self.quit
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button == self.start:
@@ -22,13 +28,19 @@ class HomePage(Screen):
             self.exit()
 
 class Search(Screen):
+    CSS_PATH = "style.tcss"
     def compose(self):
         self.backToHome = Button("Back")
         yield self.backToHome
-        self.searchError = Label()
-        yield self.searchError
-        self.searchInput = Input(placeholder="Who's That Pokemon?!")
-        yield self.searchInput
+        self.searchAscii = Static(" : :. : : : :. : : : :. : : : :. : : : :. : : : :. : : : :. \n : :. : : : :. .@@@@@@@@# : : :. : : : :. : : : :. : : : :. \n : :. : : :@@@@%.: : :..*@@@@ :. : : : :. : : : :. : : : :. \n: : .: :.@@* .: : : : .: : ::@@@: : : : .: : : : .: @@. : .:\n: : .::@@: : .: : : : .: : : : %@@: : : .: : : :@@@=@:: : .:\n : :.@@.: : :. : : : :. : : : :. @@  : :. : :@@@:. @#: : :. \n : :@@: : : :. :@@@@@@@@  : : @@@@ : : :.%@@# : :.@@ : : :. \n : @@ : : : :=@@%: : :.*@@@@@@=. : : :@@@.: : : :@@: : : :. \n: %@.: : : :@@: :%@@@@@. :@: : .: *@@@: .: : : :@@: : : : .:\n: @@.: : : @@.:@@*: : =@@. : : @@@: : : .: : : @@.: : : : .:\n :@ . : : #@ .@@ : : :. @@: @@%. : : : :. : : @@.. : : : :. \n =@.. : : @@:.@@ : : :. :@: @@:. : : : :. : :@@ :. : : : :. \n :@ . : :@@@*.@@ : : :. %@: @@:. : : : :. : #@: :. : : : :. \n: @@.:@@@* : .:@@.: : .@@: %@. .: : : : .: +@. : .: : : : .:\n: %@@@ : : : .: -@@@@@@+ :@@ : .: : : : .::@:: : .: : : : .:\n: : .: : :%@@@@@@ : : .:@@@: : .: : : : .:@% : : .: : : : .:\n : :. :@@@= :. :%@@@@@@@: : : :. : : : :.@@ : : :. : : : :. \n : :.@@ : : :. : : : :. : : : :. : : : :@@: : : :. : : : :. \n: : .:-@@. : .: : : : .: : : : .: : : :@@: : : : .: : : : .:\n: : .: :-@@@ .: : : : .: : : : .: : : @@.: : : : .: : : : .:\n: : .: : : #@@@@. : : .: : : : .: : :@@ .: : : : .: : : : .:\n : :. : : : :. :*@@@@@@@: : : :. : :@@ :. : : : :. : : : :. \n : :. : : : :. : : : :=@@ : : :. : @@: :. : : : :. : : : :. \n. . .. . . . .. . . . .@@. . . .. %@  . .. . . . .. . . . ..\n: : .: : : : .: : : : .*@@ : : .:+@:: : .: : : : .: : : : .:\n: : .: : : : .: : : : .:@@ : : .=@% : : .: : : : .: : : : .:\n : :. : : : :. : : : :. *@@ : ::@@ : : :. : : : :. : : : :. \n : :. : : : :. : : : :. :@@ : :@@: : : :. : : : :. : : : :. \n : :. : : : :. : : : :. :=@=: @@ : : : :. : : : :. : : : :. \n: : .: : : : .: : : : .: :@@ @@.: : : : .: : : : .: : : : .:\n: : .: : : : .: : : : .: :=@@@ .: : : : .: : : : .: : : : .:\n : :. : : : :. : : : :. : :@@ :. : : : :. : : : :. : : : :. \n : :. : : : :. : : : :. : :=: :. : : : :. : : : :. : : : :. ", id="asciiSearch")
+        with Center():
+            yield self.searchAscii
+        self.searchError = Label(id="errorSearch")
+        with Center():
+            yield self.searchError
+        self.searchInput = Input(placeholder="Who's That Pokemon?!", id="inputSearch")
+        with Center():
+            yield self.searchInput
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button == self.backToHome:
@@ -37,37 +49,65 @@ class Search(Screen):
 
     def on_input_submitted(self, event):
         pokeApi = requests.get(f"https://pokeapi.co/api/v2/pokemon/{event.value}")
-        if pokeApi.status_code == 200:
+        if pokeApi.status_code == 200: #status 200 means the Pokemon was found successfully
             pokeData = json.loads(pokeApi.text)
-            asciiData = get_pokemon(pokemons=asciiPokemon, name=event.value)
+            asciiData = get_pokemon(pokemons=asciiPokemon, name=event.value) #looks up ascii art matching the searched Pokemon name
             for key in asciiData:
-                asciiDataKey = asciiData[key]
-                searchResults = Results(asciiDataKey['ascii'], pokeData['name'])
+                asciiDataKey = asciiData[key] #ascii data is nested under the Pokemon's ID number as the dict key
+                searchResults = Results(asciiDataKey['ascii'], pokeData['name'], pokeData['stats']) #passing fetched data into Results so it can display without re-fetching
                 self.app.push_screen(searchResults)
         else:
-            self.searchError.update(f"Uhhh Wrong Pokemon?...")
+            self.searchError.update(f"Uhhh Wrong Pokemon?...") #error handling added for incorrect end-user input ensuring no crashes
 
 class Results(Screen):
-    def __init__ (self, searchAscii, searchPokemon):
+    def __init__ (self, searchAscii, searchPokemon, statsData): #storing data passed in from Search for use in compose()
         super().__init__()
-        self.resultsAscii = searchAscii
-        self.resultsPokemon = searchPokemon
+        self.searchAscii = searchAscii
+        self.searchPokemon = searchPokemon
+        self.statsData = statsData
 
     def compose(self):
         self.backToSearch = Button("Back")
         yield self.backToSearch
-        self.resultsAsciiWidget = Static(self.resultsAscii)
-        yield self.resultsAsciiWidget
-        self.resultsPokemonWidget = Static(f"You Chose: {self.resultsPokemon}!")
-        yield self.resultsPokemonWidget
+        self.resultsAsciiWidget = Static(self.searchAscii, id="asciiResults")
+        with Center():
+            yield self.resultsAsciiWidget
+        self.resultsPokemonWidget = Static(f"You Chose: {self.searchPokemon.capitalize()}!", id="pokemonResults")
+        with Center():
+            yield self.resultsPokemonWidget
+        self.statsButton = Button("Stats")
+        with Center():
+                yield self.statsButton
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button == self.backToSearch:
             self.app.push_screen('search')
+        elif event.button == self.statsButton:
+            sendStats = Stats(self.statsData)
+            self.app.push_screen(sendStats) #sending data to stats screen
+
+class Stats(Screen):
+    def __init__(self, searchPokemonStats):
+        super().__init__()
+        self.searchPokemonStats = searchPokemonStats
+
+    def compose(self):
+        self.backToResults = Button("Back")
+        yield self.backToResults
+        pokeStats = ""
+        for stats in self.searchPokemonStats:
+            pokeStats += stats["stat"]["name"] + ": " + str(stats["base_stat"]) + "\n" #builds one combined string since static widgets only accept a single string, not a list
+        self.statsWidget = Static(pokeStats)
+        with Center():
+            yield self.statsWidget
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button == self.backToResults:
+            self.app.push_screen('search')
 
 
 class PokedexApp(App):
-    SCREENS = {"homepage":HomePage, "search":Search}
+    SCREENS = {"homepage":HomePage, "search":Search, "results":Results, "stats":Stats}
     def on_mount(self) -> None:
       self.push_screen('homepage')
 
